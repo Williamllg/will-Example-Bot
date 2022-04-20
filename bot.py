5d06345388b976c3a119751693d58669c1dd8bc7
Williamllg <85468792+Williamllg@users.noreply.github.com>
2022-04-20 05:09:19 +0200

    Add files via upload
    Agregando : Archivo con el codigo fuente del Bot

---
+++ bot.py
@@ -0,0 +1,19 @@
+from pip import main
+from telegram.ext import Updater , CommandHandler 
+
+# Agregano un Handler que no es  mas que las acciones que le van entrando las demas personas 
+
+def start(update , context):
+
+    update.message.reply_text("Hola Humano")
+
+if __name__== '__main__':
+    updater = Updater(token='tu_token', use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start" , start))
+    # Agregando un Handler de Comando 
+
+    # Esto es para que el bot se quede escuchando las ordenes que le van entranado por los parameteos 
+    updater.start_polling()
+    updater.idle()
